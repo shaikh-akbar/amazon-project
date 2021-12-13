269fd616f0a3afa25b3ba473c2e51476245dbb0b
shaikh-akbar <akbar000785@gmail.com>
2021-12-13 16:53:30 +0530

    init

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal";
 
 
 
 function Checkout() {
+const [{cart},dispatch] = useStateValue();
     return (
         <div className="Checkout">
             <div className="Checkout--left">
                 <img className="Checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
                 <div>
                     <h3 className="Checkout--title">Your Shopping Details</h3>
+                    {cart.map(item=>(
+                        <CheckoutProduct
+                        id = {item.id}
+                        title =  {item.title}
+                        image = {item.image}
+                        price = {item.price}
+                        />
+                    ))}
+                    {/* {CheckoutProduct} */}
+                    {/* {CheckoutProduct} */}
                 </div>
 
             </div>
 
             <div className="Checkout--right">
                 <Subtotal />
-
-
             </div>
 
         </div>
